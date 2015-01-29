d2be15aeb1feff953f899a56061dad8784a0ddda
k0f <krystof.pesek@gmail.com>
2015-01-29 23:10:40 +0100

    change @ production machineThu Jan 29 23:10:40 CET 2015

--- zahrada.sh
+++ zahrada.sh
@@ -15,7 +15,7 @@ xrandr --output VGA-0 --mode 1280x800 --rotate left --right-of HDMI-0
 
 #(sleep 15s && cd /home/kof/Downloads/catGame_linuBuild_ver1_0 && ./catGame-3-linux.x86_64 -popupwindow -screen-width 1600 -screen-height 1280) &
 
-(sleep 1s && (cd /home/kof && ./updateGame.sh && (sleep 10s && xdotool key --repeat 5 --repeat-delay 100 Tab key Return)) || (sleep 15s && cd /home/kof/Downloads/catGame_linuBuild_ver1_0 && ./catGame-3-linux.x86_64 -popupwindow -screen-width 1600 -screen-height 1280) && (sleep 20s && xdotool key --repeat 5 --repeat-delay 100 Tab key Return)) &
+(sleep 1s && (cd /home/kof && ./updateGame.sh && (sleep 10s && playx)) || (sleep 15s && cd /home/kof/Downloads/catGame_linuBuild_ver1_0 && ./catGame-3-linux.x86_64 -popupwindow -screen-width 1600 -screen-height 1280) && (sleep 10s && playx)) &
 
 #
 #(sleep 15s && (cd /home/kof && ./updateGame.sh && (sleep 60s && xdotool key --repeat 5 --repeat-delay 100 Tab key Return))&
