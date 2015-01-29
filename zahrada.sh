aefef881474ebf9e462114c3a79c8688f75b1140
k0f <krystof.pesek@gmail.com>
2015-01-29 20:44:35 +0100

    change @ production machineThu Jan 29 20:44:35 CET 2015

--- zahrada.sh
+++ zahrada.sh
@@ -9,6 +9,8 @@ xrandr --output VGA-0 --mode 1280x800 --rotate left --right-of HDMI-0
 
 
 (cd /home/kof/sketchbook/instalaceMatyasPlzen/trackerGST/ && git add . ; git commit -am "change @ production machine`date`"; git pull)&
-(sleep 1s && cd /home/kof/processing-2.2.1 && ./processing-java --sketch=/home/kof/sketchbook/instalaceMatyasPlzen/trackerGST/ --output=/tmp/trash --force --run) &
+#(sleep 1s && cd /home/kof/processing-2.2.1 && ./processing-java --sketch=/home/kof/sketchbook/instalaceMatyasPlzen/trackerGST/ --output=/tmp/trash --force --run) &
+(sleep 1s && cd sketchbook/instalaceMatyasPlzen/trackerGST/application.linux/ && ./trackerGST) &
+
 (sleep 15s && cd /home/kof/Downloads/catGame_linuBuild_ver1_0 && ./catGame-3-linux.x86_64 -popupwindow -screen-width 1600 -screen-height 1280) &
 (sleep 20s && xdotool key --repeat 5 --repeat-delay 100 Tab key Return) &
