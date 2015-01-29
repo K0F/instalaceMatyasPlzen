3a9eb4e785dc71becdba54b3e6c256ad82af8714
k0f <krystof.pesek@gmail.com>
2015-01-29 20:40:20 +0100

    test

--- zahrada.sh
+++ zahrada.sh
@@ -7,6 +7,7 @@
 xrandr --output HDMI-0 --mode 1280x800 --rotate right
 xrandr --output VGA-0 --mode 1280x800 --rotate left --right-of HDMI-0
 
+
 (cd /home/kof/sketchbook/instalaceMatyasPlzen/trackerGST/ && git add . ; git commit -am "change @ production machine`date`"; git pull)&
 (sleep 1s && cd /home/kof/processing-2.2.1 && ./processing-java --sketch=/home/kof/sketchbook/instalaceMatyasPlzen/trackerGST/ --output=/tmp/trash --force --run) &
 (sleep 15s && cd /home/kof/Downloads/catGame_linuBuild_ver1_0 && ./catGame-3-linux.x86_64 -popupwindow -screen-width 1600 -screen-height 1280) &
