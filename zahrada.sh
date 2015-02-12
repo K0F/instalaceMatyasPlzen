9a43ef8cb940440fe1f6cf2373f76ca240bc9b76
k0f <krystof.pesek@gmail.com>
2015-02-12 21:27:43 +0100

    manual

--- zahrada.sh
+++ zahrada.sh
@@ -8,7 +8,7 @@ xrandr --output HDMI-0 --mode 1280x800 --rotate right
 xrandr --output VGA-0 --mode 1280x800 --rotate left --right-of HDMI-0
 
 #(sleep 1s && cd /home/kof/processing-2.2.1 && ./processing-java --sketch=/home/kof/sketchbook/instalaceMatyasPlzen/trackerGST/ --output=/tmp/trash --force --run) &
-(sleep 1s && cd sketchbook/instalaceMatyasPlzen/trackerGST/application.linux/ && ./trackerGST) &
+(sleep 1s && cd /home/kof/sketchbook/instalaceMatyasPlzen/trackerGST/application.linux/ && ./trackerGST) &
 
 #(sleep 15s && cd /home/kof/sketchbook/instalaceMatyasPlzen/trackerGST/application.linux && ./catGame-3-linux.x86_64 -popupwindow -screen-width 1600 -screen-height 1280) &
 
