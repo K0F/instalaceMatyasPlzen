274d01709b5832042e56b0f869e10509fa320e90
k0f <krystof.pesek@gmail.com>
2015-02-12 20:03:34 +0100

    change @ production machineThu Feb 12 20:03:34 CET 2015

--- updateGame.sh
+++ updateGame.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /tmp/ && wget -r --no-parent ftp://catgame:123456@192.168.0.150/catgame/ > /home/kof/sketchbook/instalaceMatyasPlzen/update.log 2>&1 && cd /tmp/192.168.0.150/catgame/ && ./catGame-3-linux.x86_64 -popupwindow -screen-width 1600 -screen-height 1280 >> /home/kof/sketchbook/instalaceMatyasPlzen/update.log 2>&1
+cd /tmp/ && wget -r --no-parent ftp://catgame:123456@192.168.0.150/catgame/ > /home/kof/sketchbook/instalaceMatyasPlzen/update.log 2>&1 && cd /tmp/192.168.0.150/catgame/ && chmod +x ./catGame-3-linux.x86_64 && ./catGame-3-linux.x86_64 -popupwindow -screen-width 1600 -screen-height 1280 >> /home/kof/sketchbook/instalaceMatyasPlzen/update.log 2>&1
