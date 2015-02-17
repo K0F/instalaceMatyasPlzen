44c62a4264d9180bf54a5ab68eaea2343d462b59
k0f <krystof.pesek@gmail.com>
2015-02-17 19:56:49 +0100

    doba

--- updateDebug.sh
+++ updateDebug.sh
@@ -3,7 +3,7 @@ export DISPLAY=:0.0
 xset -dpms
 xset s off
 
-(sleep 60s && cd /home/kof/sketchbook/instalaceMatyasPlzen/ && git add . ; git commit -am "change @ production machine`date`"; git pull; git push)&
+(sleep 15s && cd /home/kof/sketchbook/instalaceMatyasPlzen/ && git add . ; git commit -am "change @ production machine`date`"; git pull; git push)&
 cd /home/kof/ && wget -r --no-parent ftp://catgame:123456@192.168.0.150/catgame/
 # > /home/kof/sketchbook/instalaceMatyasPlzen/update.log 2>&1 && 
 cd /home/kof/192.168.0.150/catgame/
